7e848f0ee4a4b612ef102fc4b4f96d4e70aae895
Samik Saha <sahasamik@yahoo.com>
2016-12-26 17:06:05 +0000

    http server

--- server.js
+++ server.js
@@ -3,18 +3,13 @@ var request = require('request');
 
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(process.env.PORT);
 
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
